7aca15613cfdd6bcc01e20ca1275f3dfe2ccf383
NikolayM <nikolay.mashukov@adacta-group.com>
2019-10-15 22:26:54 +0300

    added vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
